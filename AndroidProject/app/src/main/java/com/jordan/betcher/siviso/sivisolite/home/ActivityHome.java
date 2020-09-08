214cce08ec201b03dc54dcb50fed3dccde49ab1b
Plum <pplluumm@users.noreply.github.com>
2020-09-08 12:23:02 -0700

    Created Layout and Attached Google Map API
    Copied the Siviso Lite layout into a new Activity and put in the key and dependancy for the google map in place.

---
+++ AndroidProject/app/src/main/java/com/jordan/betcher/siviso/sivisolite/home/ActivityHome.java
@@ -0,0 +1,18 @@
+package com.jordan.betcher.siviso.sivisolite.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jordan.betcher.siviso.sivisolite.R;
+
+public class ActivityHome extends AppCompatActivity
+{
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
