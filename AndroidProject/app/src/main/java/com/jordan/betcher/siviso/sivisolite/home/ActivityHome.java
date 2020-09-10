97d7bd87406100f5767ab1bc894d5f47ecef511e
Plum <pplluumm@users.noreply.github.com>
2020-09-10 08:55:16 -0700

    Created map
    Created the map in SivisoGoogleMap which wraps GoogleMap making only the public methods I choose accessable (right now, none)

--- AndroidProject/app/src/main/java/com/jordan/betcher/siviso/sivisolite/home/ActivityHome.java
+++ AndroidProject/app/src/main/java/com/jordan/betcher/siviso/sivisolite/home/ActivityHome.java
@@ -2,17 +2,32 @@ package com.jordan.betcher.siviso.sivisolite.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.SupportMapFragment;
 import com.jordan.betcher.siviso.sivisolite.R;
 
 public class ActivityHome extends AppCompatActivity
 {
-    
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    
+        setupMap();
+    }
+    
+    private void setupMap()
+    {
+        SupportMapFragment mapFragment =
+        (SupportMapFragment)
+        this
+        .getSupportFragmentManager()
+        .findFragmentById(R.id.homeMap);
+    
+        Context context = this;
+        SivisoGoogleMap map = new SivisoGoogleMap(this, mapFragment);
     }
 }
---
+++ AndroidProject/app/src/test/java/com/jordan/betcher/siviso/sivisolite/home/TestSivisoGoogleMap.java
@@ -0,0 +1,14 @@
+package com.jordan.betcher.siviso.sivisolite.home;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestSivisoGoogleMap
+{
+	@Test
+	public void goTo()
+	{
+	
+	}
+}
