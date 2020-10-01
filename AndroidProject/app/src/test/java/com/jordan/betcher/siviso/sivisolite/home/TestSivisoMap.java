6910135f493d6f0c9fa35f4664b86c8825e297b7
Plum <pplluumm@users.noreply.github.com>
2020-10-01 09:03:42 -0700

    Mock final class, googlemap test
    Was finally able to mock the final class GoogleMap with PowerMock

--- AndroidProject/app/src/test/java/com/jordan/betcher/siviso/sivisolite/home/TestSivisoMap.java
+++ AndroidProject/app/src/test/java/com/jordan/betcher/siviso/sivisolite/home/TestSivisoMap.java
@@ -6,11 +6,21 @@ import com.jordan.betcher.siviso.sivisolite.home.sivisomap.OnMapClickListenerSin
 import com.jordan.betcher.siviso.sivisolite.home.sivisomap.SivisoMap;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.robolectric.annotation.Config;
 
 import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+@RunWith(PowerMockRunner.class)
+@Config(manifest = Config.NONE)
+@PrepareForTest(GoogleMap.class)
+@PowerMockIgnore({ "org.mockito.*", "org.robolectric.*", "android.*", "androidx.*" })
 public class TestSivisoMap
 {
 	@Test
@@ -25,7 +35,7 @@ public class TestSivisoMap
 	public void SivisoMap_OnMapCallback_addOnMapClickListener()
 	{
 		SupportMapFragment fakeSupportMapFragment = mock(SupportMapFragment.class);
-		GoogleMap fakeGoogleMap = mock(GoogleMap.class);
+		GoogleMap fakeGoogleMap = PowerMockito.mock(GoogleMap.class);
 		SivisoMap sivisoMap = new SivisoMap(fakeSupportMapFragment);
 		sivisoMap.onMapReady(fakeGoogleMap);
 		verify(fakeGoogleMap).setOnMapClickListener(isA(OnMapClickListenerSingleCircle.class));
