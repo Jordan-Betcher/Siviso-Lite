1e981f59f0772ec0f56a5a67b2e7c79d6afcd830
Plum <pplluumm@users.noreply.github.com>
2020-10-19 12:31:36 -0700

    Map goToLocation
    Map now has goToLocation public method and tests

--- AndroidProject/app/src/main/java/com/jordan/betcher/siviso/sivisolite/thirdparty/googlemap/Map.java
+++ AndroidProject/app/src/main/java/com/jordan/betcher/siviso/sivisolite/thirdparty/googlemap/Map.java
@@ -1,6 +1,11 @@
 package com.jordan.betcher.siviso.sivisolite.thirdparty.googlemap;
 
+import android.location.Location;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
 
 public class Map
 {
@@ -17,13 +22,19 @@ public class Map
 		googleMap.setOnMapClickListener(wrapper);
 	}
 	
-	/*
 	public void goToLocation(Location location)
 	{
 		double lat = location.getLatitude();
 		double lng = location.getLongitude();
 		LatLng latLng = new LatLng(lat, lng);
-		googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-	}//*/
+		CameraUpdate moveToLocation = locationCameraUpdate(latLng);
+		googleMap.moveCamera(moveToLocation);
+	}
+	
+	protected CameraUpdate locationCameraUpdate(LatLng latLng)
+	{
+		CameraUpdate moveToLocation = CameraUpdateFactory.newLatLng(latLng);
+		return moveToLocation;
+	}
 }
 
--- AndroidProject/app/src/test/java/com/jordan/betcher/siviso/sivisolite/thirdparty/googlemap/Test$Map.java
+++ AndroidProject/app/src/test/java/com/jordan/betcher/siviso/sivisolite/thirdparty/googlemap/Test$Map.java
@@ -1,6 +1,10 @@
 package com.jordan.betcher.siviso.sivisolite.thirdparty.googlemap;
 
+import android.location.Location;
+
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,12 +14,14 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.robolectric.annotation.Config;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(PowerMockRunner.class)
 @Config(manifest = Config.NONE)
-@PrepareForTest(GoogleMap.class)
+@PrepareForTest({GoogleMap.class, CameraUpdate.class})
 @PowerMockIgnore({ "org.mockito.*", "org.robolectric.*", "android.*", "androidx.*" })
 public class Test$Map
 {
@@ -24,10 +30,66 @@ public class Test$Map
 	{
 		GoogleMap fakeGoogleMap = PowerMockito.mock(GoogleMap.class);
 		MapClick fakeOnMapClick = PowerMockito.mock(MapClick.class);
-		Map wrapperGoogleMap = new Map(fakeGoogleMap);
+		Map map = new Map(fakeGoogleMap);
 		
-		wrapperGoogleMap.setOnMapClickListener(fakeOnMapClick);
+		map.setOnMapClickListener(fakeOnMapClick);
 		
 		verify(fakeGoogleMap).setOnMapClickListener(isA(GoogleMap.OnMapClickListener.class));
+	}
+	
+	@Test
+	public void goToLocation_fakeLocation_CalledCameraWithLocation()
+	{
+		GoogleMap fakeGoogleMap = PowerMockito.mock(GoogleMap.class);
+		Map map = new Map$CameraUpdate(fakeGoogleMap);
+		
+		Location fakeLocation = PowerMockito.mock(Location.class);
+		map.goToLocation(fakeLocation);
+		
+		verify(fakeGoogleMap).moveCamera(isA(CameraUpdate.class));
+	}
+	
+	@Test
+	public void goToLocation_fakeLocation00_CalledLocationCameraUpdateWithLatLng00()
+	{
+		GoogleMap fakeGoogleMap = PowerMockito.mock(GoogleMap.class);
+		Map$CameraUpdate map = new Map$CameraUpdate(fakeGoogleMap);
+		
+		Location fakeLocation = PowerMockito.mock(Location.class);
+		when(fakeLocation.getLatitude()).thenReturn(0.0);
+		when(fakeLocation.getLongitude()).thenReturn(0.0);
+		map.goToLocation(fakeLocation);
+		
+		assertEquals(map.latLng, new LatLng(0.0, 0.0));
+	}
+	
+	@Test
+	public void goToLocation_fakeLocation11_CalledLocationCameraUpdateWithLatLng11()
+	{
+		GoogleMap fakeGoogleMap = PowerMockito.mock(GoogleMap.class);
+		Map$CameraUpdate map = new Map$CameraUpdate(fakeGoogleMap);
+		
+		Location fakeLocation = PowerMockito.mock(Location.class);
+		when(fakeLocation.getLatitude()).thenReturn(1.0);
+		when(fakeLocation.getLongitude()).thenReturn(1.0);
+		map.goToLocation(fakeLocation);
+		
+		assertEquals(map.latLng, new LatLng(1.0, 1.0));
+	}
+	
+	private class Map$CameraUpdate extends Map
+	{
+		public LatLng latLng;
+		public Map$CameraUpdate(GoogleMap googleMap)
+		{
+			super(googleMap);
+		}
+		
+		@Override
+		protected CameraUpdate locationCameraUpdate(LatLng latLng)
+		{
+			this.latLng = latLng;
+			return PowerMockito.mock(CameraUpdate.class);
+		}
 	}
 }
