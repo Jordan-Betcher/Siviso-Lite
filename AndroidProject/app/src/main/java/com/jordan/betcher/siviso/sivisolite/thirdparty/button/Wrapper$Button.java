50e9e8ecb355283d4dccd7cad776239ce782396e
Plum <pplluumm@users.noreply.github.com>
2020-10-22 10:15:18 -0700

    Test: Wrapper$Button

---
+++ AndroidProject/app/src/main/java/com/jordan/betcher/siviso/sivisolite/thirdparty/button/Wrapper$Button.java
@@ -0,0 +1,24 @@
+package com.jordan.betcher.siviso.sivisolite.thirdparty.button;
+
+import android.view.View;
+import android.widget.Button;
+
+public class Wrapper$Button
+{
+	private Button button;
+	
+	public Wrapper$Button(Button button)
+	{
+		this.button = button;
+	}
+	
+	public void addButtonClick(View.OnClickListener onClickListener)
+	{
+		button.setOnClickListener(onClickListener);
+	}
+	
+	public void disable()
+	{
+		button.setEnabled(false);
+	}
+}
