d4f886acd570814bd2395708156ddf3b9236e316
Plum <pplluumm@users.noreply.github.com>
2021-01-18 14:59:05 -0800

    Moved CircleOptions into Highlight and there is only 1 highlight circle at a time

--- AndroidProject/app/src/main/java/com/jordan/betcher/siviso/sivisolite/home/onMapReady/OnMapClick$HighlightLocation.java
+++ AndroidProject/app/src/main/java/com/jordan/betcher/siviso/sivisolite/home/onMapReady/OnMapClick$HighlightLocation.java
@@ -1,8 +1,6 @@
 package com.jordan.betcher.siviso.sivisolite.home.onMapReady;
 
-import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.jordan.betcher.siviso.sivisolite.R;
 import com.jordan.betcher.siviso.sivisolite.thirdparty.googlemap.Map;
 import com.jordan.betcher.siviso.sivisolite.thirdparty.googlemap.OnMapClick;
 import com.jordan.betcher.siviso.sivisolite.thirdparty.googlemap.Wrapper$Circle;
@@ -10,32 +8,23 @@ import com.jordan.betcher.siviso.sivisolite.thirdparty.googlemap.Wrapper$Circle;
 class OnMapClick$HighlightLocation
 implements OnMapClick
 {
-	Wrapper$Circle circle;
-	CircleOptions highlight;
+	Highlight highlight = new Highlight();
+	Wrapper$Circle circle = null;
 	private Map map;
 	
 	public OnMapClick$HighlightLocation(Map map)
 	{
 		this.map = map;
-		this.highlight = createHighlight();
 	}
 	
 	@Override
 	public void onMapClick(LatLng latLng)
 	{
-		highlight.center(latLng);
+		if(circle != null)
+		{
+			circle.remove();
+		}
 		
-		circle = map.createCircle(highlight);
-	}
-	
-	private CircleOptions createHighlight()
-	{
-		CircleOptions highlight = new CircleOptions();
-		
-		highlight.fillColor(R.color.highlight);
-		highlight.radius(R.integer.highlightRadius);
-		highlight.visible(true);
-		
-		return highlight;
+		circle = highlight.createCircle(map, latLng);
 	}
 }
--- AndroidProject/app/src/test/java/com/jordan/betcher/siviso/sivisolite/home/onMapReady/Test$OnMapClick$HighlightLocation.java
+++ AndroidProject/app/src/test/java/com/jordan/betcher/siviso/sivisolite/home/onMapReady/Test$OnMapClick$HighlightLocation.java
@@ -2,113 +2,21 @@ package com.jordan.betcher.siviso.sivisolite.home.onMapReady;
 
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.jordan.betcher.siviso.sivisolite.ArgumentMatcher$Same;
-import com.jordan.betcher.siviso.sivisolite.R;
 import com.jordan.betcher.siviso.sivisolite.thirdparty.googlemap.Map;
 import com.jordan.betcher.siviso.sivisolite.thirdparty.googlemap.Wrapper$Circle;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-import static org.mockito.Matchers.argThat;
 import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class Test$OnMapClick$HighlightLocation
 {
-	@Test
-	public void onMapClick_latLng_callhighlightWithCircleOptionMatchingLatLng()
-	{
-		Map map = mock(Map.class);
-		OnMapClick$HighlightLocation onMapClick = new OnMapClick$HighlightLocation(map);
-		LatLng latLng = new LatLng(0,0);
-		
-		onMapClick.onMapClick(latLng);
-		
-		CircleOptions circleOptions = new CircleOptions();
-		circleOptions.center(latLng);
-		verify(map).createCircle(argThat(new Same$CircleOptionLatLng(circleOptions)));
-	}
-	
-	
-	private class Same$CircleOptionLatLng extends ArgumentMatcher$Same<CircleOptions>
-	{
-		public Same$CircleOptionLatLng(CircleOptions first)
-		{
-			super(first);
-		}
-		
-		@Override
-		protected boolean isSameValues(CircleOptions other)
-		{
-			return first.getCenter() == other.getCenter();
-		}
-	}
-	
-	
-	@Test
-	public void onMapClick_latLng_callhighlightWithCircleOptionMatchingRadius()
-	{
-		Map map = mock(Map.class);
-		OnMapClick$HighlightLocation onMapClick = new OnMapClick$HighlightLocation(map);
-		LatLng latLng = new LatLng(0,0);
-		
-		onMapClick.onMapClick(latLng);
-		
-		CircleOptions circleOptions = new CircleOptions();
-		circleOptions.radius(R.integer.highlightRadius);
-		verify(map).createCircle(argThat(new Same$CircleOptionRadius(circleOptions)));
-	}
-	
-	
-	private class Same$CircleOptionRadius extends ArgumentMatcher$Same<CircleOptions>
-	{
-		public Same$CircleOptionRadius(CircleOptions first)
-		{
-			super(first);
-		}
-		
-		@Override
-		protected boolean isSameValues(CircleOptions other)
-		{
-			return first.getRadius() == other.getRadius();
-		}
-	}
-	
-	@Test
-	public void onMapClick_latLng_callhighlightWithCircleOptionMatchingFillColor()
-	{
-		Map map = mock(Map.class);
-		OnMapClick$HighlightLocation onMapClick = new OnMapClick$HighlightLocation(map);
-		LatLng latLng = new LatLng(0,0);
-		
-		onMapClick.onMapClick(latLng);
-		
-		CircleOptions circleOptions = new CircleOptions();
-		circleOptions.fillColor(R.color.highlight);
-		verify(map).createCircle(argThat(new Same$CircleOptionFillColor(circleOptions)));
-	}
-	
-	
-	private class Same$CircleOptionFillColor extends ArgumentMatcher$Same<CircleOptions>
-	{
-		public Same$CircleOptionFillColor(CircleOptions first)
-		{
-			super(first);
-		}
-		
-		@Override
-		protected boolean isSameValues(CircleOptions other)
-		{
-			return first.getFillColor() == other.getFillColor();
-		}
-	}
-	
-	
 	@Test
 	public void onMapClick__circleCreated()
 	{
@@ -120,7 +28,7 @@ public class Test$OnMapClick$HighlightLocation
 		
 		onMapClick.onMapClick(latLng);
 		
-		assertNotNull(onMapClick.circle);
+		assertEquals(circle, onMapClick.circle);
 	}
 	
 	@Test
@@ -134,26 +42,38 @@ public class Test$OnMapClick$HighlightLocation
 	}
 	
 	@Test
-	public void onMapClickTwice_differentLatLng_differentCircle()
+	public void onMapClickTwice__callRemoveOnFirstCircle()
 	{
-		
 		Wrapper$Circle circle1 = mock(Wrapper$Circle.class);
 		Wrapper$Circle circle2 = mock(Wrapper$Circle.class);
 		Map map = mock(Map.class);
 		OnMapClick$HighlightLocation onMapClick = new OnMapClick$HighlightLocation(map);
-		when(map.createCircle(onMapClick.highlight)).thenReturn(circle1);
-		LatLng latLng1 = new LatLng(0,0);
-		LatLng latLng2 = new LatLng(1,1);
+		LatLng latLng = new LatLng(0,0);
 		
-		onMapClick.onMapClick(latLng1);
+		when(map.createCircle(isA(CircleOptions.class))).thenReturn(circle1);
+		onMapClick.onMapClick(latLng);
 		
-		Wrapper$Circle actualCircle1 = onMapClick.circle;
+		when(map.createCircle(isA(CircleOptions.class))).thenReturn(circle2);
+		onMapClick.onMapClick(latLng);
 		
-		when(map.createCircle(onMapClick.highlight)).thenReturn(circle2);
-		onMapClick.onMapClick(latLng2);
+		verify(circle1, times(1)).remove();
+	}
+	
+	@Test
+	public void onMapClickTwice__notCallRemoveOn2ndCircle()
+	{
+		Wrapper$Circle circle1 = mock(Wrapper$Circle.class);
+		Wrapper$Circle circle2 = mock(Wrapper$Circle.class);
+		Map map = mock(Map.class);
+		OnMapClick$HighlightLocation onMapClick = new OnMapClick$HighlightLocation(map);
+		LatLng latLng = new LatLng(0,0);
 		
-		Wrapper$Circle actualCircle2 = onMapClick.circle;
+		when(map.createCircle(isA(CircleOptions.class))).thenReturn(circle1);
+		onMapClick.onMapClick(latLng);
 		
-		assertNotSame(actualCircle1, actualCircle2);
+		when(map.createCircle(isA(CircleOptions.class))).thenReturn(circle2);
+		onMapClick.onMapClick(latLng);
+		
+		verify(circle2, times(0)).remove();
 	}
 }
