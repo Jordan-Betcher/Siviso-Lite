f6e84a5dc1a14b2b6ce6b558d388df26ffcb3e00
Plum <pplluumm@users.noreply.github.com>
2021-09-10 11:22:16 -0700

    rt Test$LocationListener$CurrentLocation

--- AndroidProject/app/src/test/java/com/jordan/betcher/siviso/sivisolite/thirdparty/locationManager/Test$LocationListener$CurrentLocation.java
+++ AndroidProject/app/src/test/java/com/jordan/betcher/siviso/sivisolite/thirdparty/locationManager/Test$LocationListener$CurrentLocation.java
@@ -1,23 +1,18 @@
 package com.jordan.betcher.siviso.sivisolite.thirdparty.locationManager;
 
-import android.location.Location;
-import android.location.LocationListener;
-
+import static org.mockito.Mockito.*;
+import android.location.*;
 import org.junit.Test;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 public class Test$LocationListener$CurrentLocation
 {
-	
 	@Test
 	public void onLocationChanged_Location_CallCurrentLocationSetLocation()
 	{
 		Location fakeLocation = mock(Location.class);
 		CurrentLocation fakeCurrentLocation = mock(CurrentLocation.class);
-		LocationListener locationListener = new LocationListener$CurrentLocation(fakeCurrentLocation);
 		
+		LocationListener locationListener = new LocationListener$CurrentLocation(fakeCurrentLocation);
 		locationListener.onLocationChanged(fakeLocation);
 		
 		verify(fakeCurrentLocation).setCurrentLocation(fakeLocation);
