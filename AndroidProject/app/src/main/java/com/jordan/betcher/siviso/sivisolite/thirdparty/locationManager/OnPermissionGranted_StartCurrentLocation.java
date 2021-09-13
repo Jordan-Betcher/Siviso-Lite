2179b4913e54aa00baba54315daf1598b630031f
Plum <pplluumm@users.noreply.github.com>
2021-09-13 10:26:47 -0700

    t Test$OnPermissionGranted_StartCurrentLocation __0requestLocationUpdates

--- AndroidProject/app/src/main/java/com/jordan/betcher/siviso/sivisolite/thirdparty/locationManager/OnPermissionGranted_StartCurrentLocation.java
+++ AndroidProject/app/src/main/java/com/jordan/betcher/siviso/sivisolite/thirdparty/locationManager/OnPermissionGranted_StartCurrentLocation.java
@@ -10,27 +10,22 @@ import com.jordan.betcher.siviso.sivisolite.thirdparty.permissions.OnPermissionG
 public class OnPermissionGranted_StartCurrentLocation
 implements OnPermissionGranted
 {
+	private LocationManager locationManager;
+	
 	@SuppressLint("MissingPermission")
 	public OnPermissionGranted_StartCurrentLocation(
 	Resources resources, LocationManager locationManager,
 	LocationListener_CurrentLocation currentLocation)
 	{
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
-		                                       new LocationListener_CurrentLocation(new Event<LatLng>()));
+		
+		this.locationManager = locationManager;
 	}
 	
 	@SuppressLint("MissingPermission")
 	@Override
 	public void permissionGranted()
 	{
-		
-		//TODO OnPermissionGranted_StartCurrentLocation OnPermissionGranted_StartCurrentLocation
-		/*
-		float minTime = 0;
-		long minDistance = 0;
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minDistance, minTime,
-		                                       currentLocation);
-		//*/
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
+		                                       new LocationListener_CurrentLocation(new Event<LatLng>()));
 	}
-	//TODO
 }
--- AndroidProject/app/src/test/java/com/jordan/betcher/siviso/sivisolite/thirdparty/locationManager/Test$OnPermissionGranted_StartCurrentLocation.java
+++ AndroidProject/app/src/test/java/com/jordan/betcher/siviso/sivisolite/thirdparty/locationManager/Test$OnPermissionGranted_StartCurrentLocation.java
@@ -11,6 +11,18 @@ import org.junit.Test;
 public class Test$OnPermissionGranted_StartCurrentLocation
 {
 	@Test
+	public void __0requestLocationUpdates()
+	{
+		Resources resources = mock(Resources.class);
+		LocationManager locationManager = mock(LocationManager.class);
+		LocationListener_CurrentLocation currentLocation = mock(LocationListener_CurrentLocation.class);
+		
+		new OnPermissionGranted_StartCurrentLocation(resources, locationManager, currentLocation);
+		
+		verify(locationManager, times(0)).requestLocationUpdates(anyString(), anyLong(), anyFloat(), any(LocationListener_CurrentLocation.class));
+	}
+	
+	@Test
 	public void permissionGranted__requestLocationUpdatesServiceProviderGPSProvider()
 	{
 		Resources resources = mock(Resources.class);
