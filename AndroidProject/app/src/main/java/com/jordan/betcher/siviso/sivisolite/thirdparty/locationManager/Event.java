372aea1dc1fb7990257b45144ab57914a314631c
Plum <pplluumm@users.noreply.github.com>
2021-09-13 11:52:00 -0700

    t Test$Event call_0On1On2_on20

--- AndroidProject/app/src/main/java/com/jordan/betcher/siviso/sivisolite/thirdparty/locationManager/Event.java
+++ AndroidProject/app/src/main/java/com/jordan/betcher/siviso/sivisolite/thirdparty/locationManager/Event.java
@@ -1,16 +1,21 @@
 package com.jordan.betcher.siviso.sivisolite.thirdparty.locationManager;
 
+import java.util.ArrayList;
+
 public class Event<T>
 {
-	private On<T> on;
+	private ArrayList<On<T>> ons = new ArrayList<>();
 	
 	public void call(T t)
 	{
-		on.call(t);
+		for(On<T> on : ons)
+		{
+			on.call(t);
+		}
 	}
 	
 	public void add(On<T> on)
 	{
-		if(this.on == null) this.on = on;
+		ons.add(on);
 	}
 }
--- AndroidProject/app/src/test/java/com/jordan/betcher/siviso/sivisolite/thirdparty/locationManager/Test$Event.java
+++ AndroidProject/app/src/test/java/com/jordan/betcher/siviso/sivisolite/thirdparty/locationManager/Test$Event.java
@@ -8,6 +8,21 @@ import org.junit.Test;
 public class Test$Event
 {
 	@Test
+	public void call_0On1On2_on20()
+	{
+		int number = 0;
+		On<Integer> on1 = mock(On.class);
+		On<Integer> on2 = mock(On.class);
+		
+		Event<Integer> event = new Event<>();
+		event.add(on1);
+		event.add(on2);
+		event.call(number);
+		
+		verify(on2, times(1)).call(number);
+	}
+
+	@Test
 	public void call_0On1On2_on10()
 	{
 		int number = 0;
